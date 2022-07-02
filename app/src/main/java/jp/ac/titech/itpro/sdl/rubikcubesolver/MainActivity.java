ee221ff26086933e3259624aa54bbde77c3de40f
Yusuke Uchida <fantomyuu0623@gmail.com>
2022-07-02 17:28:04 +0900

    init commit

---
+++ app/src/main/java/jp/ac/titech/itpro/sdl/rubikcubesolver/MainActivity.java
@@ -0,0 +1,14 @@
+package jp.ac.titech.itpro.sdl.rubikcubesolver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
